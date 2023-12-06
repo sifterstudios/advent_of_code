ccecda5ee3f87041b5e43226e24a2a49fbe3f961
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2023-12-06 22:46:26 +0100

    2023_day2_part1

---
+++ 2023/day2/part1/part1.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("part1_input")
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			print("Couldn't close test input file")
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+
+	sumPossibleGames := 0
+	maxBlue, maxRed, maxGreen := 0, 0, 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		gameNumberAndResults := strings.Split(line, ": ")
+		gameNumberString := strings.Replace(gameNumberAndResults[0], "Game ", "", -1)
+		gameNumber, _ := strconv.Atoi(gameNumberString)
+
+		resultsString := gameNumberAndResults[1]
+		allHandfuls := strings.Split(resultsString, "; ")
+
+		for _, handful := range allHandfuls {
+			colorsInHandful := strings.Split(handful, ", ")
+			for _, color := range colorsInHandful {
+				if strings.Contains(color, "blue") {
+					numberOfCubes := getNumberOfCubes(color, "blue")
+
+					if numberOfCubes > maxBlue {
+						gameNumber = 0
+					}
+				}
+				if strings.Contains(color, "red") {
+					numberOfCubes := getNumberOfCubes(color, "red")
+
+					if numberOfCubes > maxRed {
+						gameNumber = 0
+					}
+				}
+				if strings.Contains(color, "green") {
+					numberOfCubes := getNumberOfCubes(color, "green")
+
+					if numberOfCubes > maxGreen {
+						gameNumber = 0
+					}
+				}
+			}
+		}
+
+		println(gameNumber)
+		sumPossibleGames += gameNumber
+	}
+	println(sumPossibleGames)
+}
+
+func getNumberOfCubes(numberAndColor, color string) (result int) {
+	onlyNumber := strings.Split(numberAndColor, color)
+	digit, _ := strconv.Atoi(onlyNumber[0])
+	println("Digit: ", digit)
+	println("Color: ", numberAndColor)
+	return digit
+}
