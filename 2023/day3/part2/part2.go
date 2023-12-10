3db0badf4a6cf98903022448be431ad27cf437cb
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2023-12-10 16:47:09 +0100

    2023_day3_part2 (FINALLY, but not pretty) - also removed input bc/ copyright

--- 2023/day3/part2/part2.go
+++ 2023/day3/part2/part2.go
@@ -19,81 +19,109 @@ type starEntry struct {
 }
 
 func main() {
-	file, err := os.Open("./input/day3_part2_input")
+	file, err := os.Open("./2023/input/day3_part2_input")
 	if err != nil {
 		println("Error opening file")
 	}
 
 	scanner := bufio.NewScanner(file)
-	allLines := []string{}
-	defer file.Close()
+	var allLines []string
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			println("Trouble closing the file!")
+		}
+	}(file)
 
 	for scanner.Scan() {
 		allLines = append(allLines, scanner.Text())
 	}
 
-	sumSchematics := 0
 	sumGears := 0
 
-	for i := 0; i < len(allLines); i++ {
-		sumSchematics = getSumOfValidSchematics(allLines, i, err, sumSchematics)
-	}
 	getSumOfGears(allLines, &sumGears)
-	println("Sum of schematics: ", sumSchematics)
 	println("Sum of gears: ", sumGears)
 }
 
 func getSumOfGears(allLines []string, sumGears *int) {
-	numberEntries := []numberEntry{}
-	starEntries := []starEntry{}
+	var numberEntries []numberEntry
+	var starEntries []starEntry
 
 	for i := 0; i < len(allLines); i++ {
-		for j := 0; j < len(allLines[i]); j++ {
-			if isNumber(string(allLines[i][j])) {
-				numberEntries = append(numberEntries, numberEntry{i, j, -1, -1})
-				// Now we've fonud the start of a number, find the end of it
-				for j < len(allLines[i]) && isNumber(string(allLines[i][j])) {
-					j++
-				}
-				// Was no longer a number or end of line
-				numberEntries[len(numberEntries)-1].end = j - 1 // the -1 is questionable, remember this in test
-				numberEntries[len(numberEntries)-1].fullNumber, _ = strconv.Atoi(getFullNumber(allLines[i], j-1, true))
-			}
-
-			if string(allLines[i][j]) == "*" {
-				println("Found a star at ", i, j)
+		currentLine := allLines[i]
+		for j := 0; j < len(currentLine); j++ {
+			currentChar := string(allLines[i][j])
+			if currentChar == "*" {
 				starEntries = append(starEntries, starEntry{i, j})
-
+				continue
+			}
+			numberEntries = getNumberEntryForChar(allLines, currentChar, i, &j, currentLine, numberEntries)
+			if j == len(currentLine)-1 {
+				break
 			}
 		}
 	}
-	println("Collected numbers and stars")
+
+	println("NumberEntries: ", len(numberEntries))
 	for star := 0; star < len(starEntries); star++ {
 		numbersFound := checkForNumberInAllDirections(starEntries[star], numberEntries)
 		if len(numbersFound) == 2 {
-			// Found a gear
-			*sumGears += numbersFound[0] * numbersFound[1]
+			product := numbersFound[0] * numbersFound[1]
+			*sumGears += product
+		} else if len(numbersFound) > 2 {
+			println("More than two numbers found for star #", star)
 		}
 	}
+	println("Found ", len(starEntries), " stars")
+}
+
+func getNumberEntryForChar(allLines []string, currentChar string, i int, j *int, currentLine string, numberEntries []numberEntry) []numberEntry {
+	if isNumber(currentChar) {
+		entry := numberEntry{i, *j, -1, -1}
+		for *j < len(currentLine) && isNumber(string(currentLine[*j])) {
+			if *j < len(currentLine) {
+				*j++
+			} else {
+				break
+			}
+		}
+		*j--
+		// Was no longer a number or end of line
+		entry.end = *j
+		entry.fullNumber, _ = strconv.Atoi(getFullNumber(allLines[i], entry.start))
+
+		// Add to slice
+		numberEntries = append(numberEntries, entry)
+	}
+	return numberEntries
 }
 
 func checkForNumberInAllDirections(star starEntry, numberEntries []numberEntry) (numbersFound []int) {
 	idx, line := star.idx, star.line
 
 	// ABOVE
-	above := numberInSquare(numberEntries, line-1, idx)
-	if above != -1 {
-		numbersFound = append(numbersFound, above)
+	above1, above2 := numberAboveOrBelow(numberEntries, line-1, idx)
+	if above1 != -1 {
+		numbersFound = append(numbersFound, above1)
+	}
+	if above2 != -1 && above1 != above2 {
+		numbersFound = append(numbersFound, above2)
 	}
 
-	below := numberInSquare(numberEntries, line+1, idx)
-	if below != -1 {
-		numbersFound = append(numbersFound, below)
+	below1, below2 := numberAboveOrBelow(numberEntries, line+1, idx)
+	if below1 != -1 {
+		numbersFound = append(numbersFound, below1)
+	}
+	if below2 != -1 && below1 != below2 {
+		numbersFound = append(numbersFound, below2)
 	}
 
-	sides := numberInSquare(numberEntries, line, idx)
-	if sides != -1 {
-		numbersFound = append(numbersFound, sides)
+	left, right := numberAdjacent(numberEntries, line, idx)
+	if left != -1 {
+		numbersFound = append(numbersFound, left)
+	}
+	if right != -1 {
+		numbersFound = append(numbersFound, right)
 	}
 
 	if len(numbersFound) > 2 {
@@ -103,129 +131,59 @@ func checkForNumberInAllDirections(star starEntry, numberEntries []numberEntry) 
 	return numbersFound
 }
 
-func numberInSquare(numberEntries []numberEntry, line int, idx int) (number int) {
+func numberAboveOrBelow(numberEntries []numberEntry, line int, idx int) (number1, number2 int) {
+	number1 = -1
+	number2 = -1
 	for number := 0; number < len(numberEntries); number++ {
 		numberEntry := numberEntries[number]
 		if numberEntry.line == line {
 			for i := idx - 1; i <= idx+1; i++ {
-				if numberEntry.start <= i && i <= numberEntry.end {
-					return numberEntry.fullNumber
+				if i >= numberEntry.start && i <= numberEntry.end {
+					if number1 != -1 {
+						number2 = numberEntry.fullNumber
+						continue
+					} else {
+						number1 = numberEntry.fullNumber
+						continue
+					}
 				}
 			}
 		}
 	}
-	return -1
+	return number1, number2
 }
 
-func getSumOfValidSchematics(allLines []string, i int, err error, sumSchematics int) int {
-	line := allLines[i]
-	for j := 0; j < len(line); j++ {
-		sideHit, aboveHit, belowHit := false, false, false
-		if isPeriod(string(line[j])) {
-			continue
-		}
-
-		fullNumberString := getFullNumber(line, j, false)
-
-		fullNumber := 0
-		if fullNumberString != "" {
-			fullNumber, err = strconv.Atoi(fullNumberString)
-			if err != nil {
-				println("Error converting string to int", err.Error())
+func numberAdjacent(numberEntries []numberEntry, line int, idx int) (left, right int) {
+	left = -1
+	right = -1
+	for number := 0; number < len(numberEntries); number++ {
+		numberEntry := numberEntries[number]
+		if numberEntry.line == line {
+			if idx-1 == numberEntry.end {
+				left = numberEntry.fullNumber
 			}
-		}
-
-		if fullNumberString != "" && !isPeriod(fullNumberString) {
-			sideHit = checkForSymbolToTheSides(j, len(fullNumberString), line)
-
-			if i != len(allLines)-1 {
-				belowHit, _ = checkForSymbolAboveOrBelow(i, j, len(fullNumberString), allLines[i+1])
-			}
-
-			if i != 0 {
-				aboveHit, _ = checkForSymbolAboveOrBelow(i, j, len(fullNumberString), allLines[i-1])
-			}
-
-			j += len(fullNumberString)
-		}
-
-		if sideHit || aboveHit || belowHit {
-			sumSchematics += fullNumber
-		}
-	}
-	return sumSchematics
-}
-
-func checkForSymbolAboveOrBelow(lineNumber, start, end int, line string) (isSymbol, isStar bool) {
-	for i := start - 1; i <= start+end; i++ {
-		if i < 0 || i >= len(line) {
-			continue
-		}
-
-		if !isPeriod(string(line[i])) && !isNumber(string(line[i])) {
-			if string(line[i]) == "*" {
-				isSymbol = true
-				isStar = true
-			} else {
-				isSymbol = true
+			if idx+1 == numberEntry.start {
+				right = numberEntry.fullNumber
 			}
 		}
 	}
-	return isSymbol, isStar
+	return left, right
 }
 
-func checkForSymbolToTheSides(start, end int, line string) bool {
-	if start != 0 {
-		if !isPeriod(string(line[start-1])) && !isNumber(string(line[start-1])) {
-			return true
-		}
-	}
-	if start+end < len(line) {
-		if !isPeriod(string(line[start+end])) && !isNumber(string(line[start+end])) {
-			return true
-		}
-	}
-	return false
-}
-
-func getFullNumber(line string, idx int, backwards bool) (fullNumber string) {
+func getFullNumber(line string, idx int) (fullNumber string) {
 	fullNumber = ""
 	for isNumber(string(line[idx])) {
 		fullNumber += string(line[idx])
 
-		if backwards && idx <= 0 {
+		idx++
+		if idx >= len(line) {
 			break
 		}
-		if !backwards && idx >= len(line) {
-			break
-		}
-		if backwards {
-			idx--
-		} else {
-			idx++
-		}
 	}
-	if backwards {
-		// reverse string
-		return Reverse(fullNumber)
-	} else {
-		return fullNumber
-	}
+	return fullNumber
 }
 
 func isNumber(s string) bool {
 	_, err := strconv.Atoi(s)
 	return err == nil
-}
-
-func isPeriod(s string) bool {
-	return s == "."
-}
-
-func Reverse(s string) string {
-	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
-	}
-	return string(r)
 }
